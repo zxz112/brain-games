b97a5db5b2116f59866751e12a43b6e93a82b7fa
Ilya VVV <Ilya@mail.ru>
2019-12-29 19:34:31 +0300

    step 8

---
+++ src/games/Gcd.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace  BrainGames\games\Gcd;
+
+use function cli\line;
+use function cli\prompt;
+use function BrainGames\Core\run;
+
+const GAMES = 'Find the greatest common divisor of given numbers.';
+
+function delit($rand1)
+{
+    $res = [];
+    $del = 1;
+    while ($rand1 != $del) {
+        if ($rand1 % $del == 0) {
+            $res[] = $del;
+        }
+        $del = $del + 1;
+    }
+    $res[] = $rand1;
+    return $res;
+}
+
+function obshi($rand1, $rand2)
+{
+    $res1 = delit($rand1);
+    $res2 = delit($rand2);
+    $result = array_uintersect($res1, $res2, "strcasecmp");
+    return max($result);
+
+}
+function gcd() 
+{
+    $answer = [];
+    $que = [];
+    
+    for ($i = 0; $i < 3; $i++) {
+        $rand1 = rand(0, 100);
+        $rand2 = rand(0, 100);
+        $que[] = "{$rand1} {$rand2}";
+        $answer[] = obshi($rand1, $rand2);
+    }
+    run($que, $answer, GAMES);
+}
---
+++ src/games/Progression.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace  BrainGames\games\Progression;
+
+use function cli\line;
+use function cli\prompt;
+use function BrainGames\Core\run;
+
+const GAMES = 'What number is missing in the progression?';
+function makeProgression($beginProgress, $progression)
+{
+    $arrProg = [];
+    $temp = $beginProgress;
+    for ($i = 0; $i < 10; $i++) {
+        $temp = $temp + $progression;
+        $arrProg[$i] = $temp;
+    }
+    return $arrProg;
+}
+function progression() 
+{
+    $answer = [];
+    $que = [];
+    for ($i = 0; $i < 3; $i++) {
+        $beginProgress = rand(0, 100);
+        $progression = rand(1, 10);
+        $randPosition = rand(0, 9);
+        $arrProg = makeProgression($beginProgress, $progression);
+        $answer[] = $arrProg[$randPosition];
+        $arrProg[$randPosition] = '..';
+        $que[] = implode($arrProg, ' ');
+    }
+    run($que, $answer, GAMES);
+}
